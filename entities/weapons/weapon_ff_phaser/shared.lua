70a3d49d8f2ac60f7a6d0120346647429f73189f
Metapyziks <metapyziks@gmail.com>
2014-07-15 17:16:49 +0100

    Prototyping a new phaser design

--- entities/weapons/weapon_ff_phaser/shared.lua
+++ entities/weapons/weapon_ff_phaser/shared.lua
@@ -1,132 +1,122 @@
--- Credit to Prof. van Fuck (steamcommunity.com/id/profvonfuck) for the original code and the particle effect.
-
-SWEP.Spawnable = true
-SWEP.AdminSpawnable = true
-
-SWEP.Author = "0x5B"
-SWEP.Contact = ""
-SWEP.Purpose = "A standard laser pistol given to all ship crew."
-SWEP.Instructions = "Point at your target, and press firmly on the trigger."
+-- Copyright (c) 2014 James King [metapyziks@gmail.com]
+-- 
+-- This file is part of GMTools.
+-- 
+-- GMTools is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as
+-- published by the Free Software Foundation, either version 3 of
+-- the License, or (at your option) any later version.
+-- 
+-- GMTools is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with GMTools. If not, see <http://www.gnu.org/licenses/>.
 
 if CLIENT then
-
 	SWEP.HoldType      = "pistol"
 	SWEP.PrintName     = "Phaser"
 	SWEP.ViewModelFOV  = 56
 	SWEP.ViewModelFlip = false
 	SWEP.Slot          = 1
 	SWEP.SlotPos       = 1
-	SWEP.DrawAmmo      = true
+	SWEP.DrawAmmo      = false
 	SWEP.DrawCrosshair = true
 	SWEP.UseHands      = true
-	
-	killicon.Add( "weapon_laserpistol", "pistolkill/pistolkill", color_white )
 end
 
 SWEP.ViewModel = "models/weapons/c_pistol.mdl"
 SWEP.WorldModel = "models/weapons/w_pistol.mdl"
 
-SWEP.Primary.Sound             = Sound("Weapon_AR2.Single")
-SWEP.Primary.Recoil            =  1
-SWEP.Primary.Damage            =  12
-SWEP.Primary.NumShots          =  1  
-SWEP.Primary.Delay             =  0.09
-SWEP.Primary.Ammo              = "Pistol"  
-SWEP.Primary.Force             = 2
+SWEP.Primary.Sound    = Sound("Weapon_AR2.Single")
+SWEP.Primary.Recoil   = 2
+SWEP.Primary.Damage   = 50
+SWEP.Primary.NumShots = 1  
+SWEP.Primary.Delay    = 0.125
+SWEP.Primary.Ammo     = "none"  
+SWEP.Primary.Force    = 2
 
-SWEP.Primary.ClipSize          = 32
-SWEP.Primary.DefaultClip       = 192
-SWEP.Primary.Automatic         = false
+SWEP.Primary.ChargeDecay  = 0.95
+SWEP.Primary.RechargeRate = 0.25
+SWEP.Primary.ClipSize     = 1
+SWEP.Primary.DefaultClip  = 1
+SWEP.Primary.Automatic    = true
 
 SWEP.ReloadSound = "Weapon_Pistol.Reload"
 
-game.AddParticles("particles/laserp_particles.pcf")
-	for _, particle in pairs({
-	"Weapon_LaserP_Beam"
-	}) do
-	PrecacheParticleSystem("")
+SWEP._lastShot = 0
+SWEP._lastCharge = 0
+
+function SWEP:GetCharge()
+	return math.Clamp(self._lastCharge + (CurTime() - self._lastShot) * self.Primary.RechargeRate, 0, 1)
 end
 
-
-function SWEP:DispatchEffect(EFFECTSTR)
-	local pPlayer=self.Owner;
-	local view;
-	if CLIENT then view=GetViewEntity() else view=pPlayer:GetViewEntity() end
-		if ( !pPlayer:IsNPC() && view:IsPlayer() ) then
-			ParticleEffectAttach( EFFECTSTR, PATTACH_POINT_FOLLOW, pPlayer:GetViewModel(), pPlayer:GetViewModel():LookupAttachment( "muzzle" ) );
-		else
-			ParticleEffectAttach( EFFECTSTR, PATTACH_POINT_FOLLOW, pPlayer, pPlayer:LookupAttachment( "anim_attachment_rh" ) );
-		end
-end
-
-function SWEP:ShootEffect(EFFECTSTR,startpos,endpos)
-	local pPlayer=self.Owner;
-	local view;
-	if CLIENT then view=GetViewEntity() else view=pPlayer:GetViewEntity() end
-		if ( !pPlayer:IsNPC() && view:IsPlayer() ) then
-			util.ParticleTracerEx( EFFECTSTR, self.Weapon:GetAttachment( self.Weapon:LookupAttachment( "muzzle" ) ).Pos,endpos, true, pPlayer:GetViewModel():EntIndex(), pPlayer:GetViewModel():LookupAttachment( "muzzle" ) );
-		else
-			util.ParticleTracerEx( EFFECTSTR, pPlayer:GetAttachment( pPlayer:LookupAttachment( "anim_attachment_rh" ) ).Pos,endpos, true,pPlayer:EntIndex(), pPlayer:LookupAttachment( "anim_attachment_rh" ) );
-		end
+function SWEP:Deploy()
+	self._lastShot = CurTime()
+	self._lastCharge = 0
 end
 
 function SWEP:PrimaryAttack()
-if ( !self:CanPrimaryAttack() ) then return end
+	if not self:CanPrimaryAttack() then return end
 
-	self:TakePrimaryAmmo(1)
-	
-	self.Weapon:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
-	self.Weapon:SetNextSecondaryFire( CurTime() + self.Primary.Delay )
-	self.Weapon:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
-	self.Owner:MuzzleFlash()
-	self.Owner:SetAnimation(PLAYER_ATTACK1)
+	local firstTime = SERVER or CurTime() - self._lastShot >= self.Primary.Delay / 2
 
-	self.Owner:ViewPunch( Angle(math.Rand(-0.4,-0.2)*self.Primary.Recoil,math.Rand(-0.3,0.2)*self.Primary.Recoil, 0))
+	local charge = self:GetCharge()
+
+	if firstTime then
+		self._lastShot = CurTime()
+		self._lastCharge = charge * charge * self.Primary.ChargeDecay
+
+		local nextShot = self._lastShot + self.Primary.Delay * (4 - 3 * charge)
+
+		self.Weapon:SetNextPrimaryFire(nextShot)
+		self.Weapon:SetNextSecondaryFire(nextShot)
+	end
+
+	self:ShootEffects()
+
+	local punchP = math.Rand(-0.4, -0.2) * self.Primary.Recoil * (1 - charge)
+	local punchY = math.Rand(-0.3,  0.2) * self.Primary.Recoil * (1 - charge)
+
+	self.Owner:ViewPunch(Angle(punchP, punchY, 0))
 	
 	local trace = {}
-		trace.start = self.Owner:GetShootPos()
-		trace.endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 20^14
-		trace.filter = self.Owner
+	trace.start = self.Owner:GetShootPos()
+	trace.endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 65536
+	trace.filter = self.Owner
+
 	local tr = util.TraceLine(trace)
 	
-	local vAng = (tr.HitPos-self.Owner:GetShootPos()):GetNormal():Angle()
+	local vAng = (tr.HitPos - self.Owner:GetShootPos()):GetNormal():Angle()
 	
-	local dmginfo = DamageInfo();
-	dmginfo:SetDamage( self.Primary.Damage );
-	dmginfo:SetAttacker( self:GetOwner() );
-	dmginfo:SetInflictor( self );
-	
-	if( dmginfo.SetDamageType ) then
-		dmginfo:SetDamagePosition( tr.HitPos );
-		dmginfo:SetDamageType( DMG_PLASMA );
+	if SERVER then
+		local dmginfo = DamageInfo()
+		dmginfo:SetDamage(self.Primary.Damage * charge)
+		dmginfo:SetAttacker(self:GetOwner())
+		dmginfo:SetInflictor(self)
+		
+		if dmginfo.SetDamageType then
+			dmginfo:SetDamagePosition(tr.HitPos)
+			dmginfo:SetDamageType(DMG_PLASMA)
+		end
+		
+		tr.Entity:DispatchTraceAttack(dmginfo, tr.HitPos, tr.HitPos - vAng:Forward() * 20)
 	end
-	
-	tr.Entity:DispatchTraceAttack( dmginfo, tr.HitPos, tr.HitPos - vAng:Forward() * 20 );
-	
-	self:ShootEffect(effect or "Weapon_LaserP_Beam",self.Owner:EyeAngles(),tr.HitPos)
 
-	local bullet = {}
-		bullet.Num = self.Primary.NumShots
-		bullet.Src = self.Owner:GetShootPos()
-		bullet.Dir = self.Owner:GetAimVector()
-		bullet.Spread = Vector( self.Primary.Cone , self.Primary.Cone, 0)
-		bullet.Tracer = 0
-		bullet.TracerName = ""
-		bullet.Force = self.Primary.Force
-		bullet.Damage = 0
-		bullet.AmmoType = self.Primary.Ammo
-	local hit1, hit2 = tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal
-	
-	local effect = EffectData()
-	effect:SetOrigin(tr.HitPos)
-	effect:SetNormal(tr.HitNormal)
-	effect:SetScale(100)
-	util.Effect("AR2Impact", effect) //add a effect
-	self:ShootEffects()
-	self.Owner:FireBullets( bullet )
-	
-	self.Weapon:EmitSound("Weapon_AR2.Single")
-	
+	if firstTime then
+		local hit1, hit2 = tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal
+		
+		local effect = EffectData()
+		effect:SetOrigin(tr.HitPos - vAng:Forward() * 4)
+		effect:SetNormal(tr.HitNormal)
+		effect:SetScale(0 * charge)
+
+		util.Effect("AR2Impact", effect)
+		
+		self.Weapon:EmitSound("weapons/ar2/fire1.wav", 100, 100 + charge * 40)
+	end
 end
 
 function SWEP:SecondaryAttack()
@@ -138,4 +128,31 @@ end
 
 function SWEP:OnRemove()
 	self:Holster()
+end
+
+if CLIENT then
+	function SWEP:DoDrawCrosshair(x, y)
+		local charge = self:GetCharge()
+		local inner = CreateHollowCircle(x, y, 24, 25, (1 - charge) * math.pi * 0.5, charge * math.pi)
+		local outer = CreateHollowCircle(x, y, 23, 26, (1 - charge) * math.pi * 0.5, charge * math.pi)
+        local clr = team.GetColor(self.Owner:Team())
+
+        draw.NoTexture()
+
+		surface.SetDrawColor(Color(clr.r, clr.g, clr.b, 64))
+		surface.DrawRect(x - 1, y - 3, 3, 6)
+        
+        for _, v in ipairs(outer) do
+			surface.DrawPoly(v)
+		end
+
+		surface.SetDrawColor(Color(255, 255, 255, 64))
+		surface.DrawRect(x, y - 2, 1, 4)
+
+        for _, v in ipairs(inner) do
+			surface.DrawPoly(v)
+		end
+
+		return true
+	end
 end

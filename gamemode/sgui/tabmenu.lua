87bdbd7fae52becdaa193c861bcfbb69e3dec508
Geroge <Spartan322@Live.com>
2015-01-05 13:44:14 -0500

    Updated LGPLv3

--- gamemode/sgui/tabmenu.lua
+++ gamemode/sgui/tabmenu.lua
@@ -1,4 +1,4 @@
--- Copyright (c) 2014 James King [metapyziks@gmail.com] and George Albany [megacake1234@gmail.com]
+-- Copyright (c) 2014 James King [metapyziks@gmail.com] and George Albany [megacake1234@gmail.com] 
 -- 
 -- This file is part of Final Frontier.
 -- 
